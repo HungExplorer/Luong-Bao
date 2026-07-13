6418d5a2a3453159baf02718a1edb28f64fc293f
Doan Quoc Hung <hungdoanvgu@gmail.com>
2026-07-13 17:37:05 +0700

    Update scheduler.c

--- src/scheduler.c
+++ src/scheduler.c
@@ -2,5 +2,6 @@
 void run_round_robin(Process proc[], int n, int quantum) {
     // Sinh viên sẽ điền code vào đây. 
     // Để test thử hệ thống hoạt động, bạn điền tạm:
+    // Bao lam bai
     proc[0].waiting_time = 6; proc[1].waiting_time = 4; proc[2].waiting_time = 7;
 }
